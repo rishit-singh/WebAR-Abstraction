0dbdde391365246c7d8108be2454ceab89fedb66
rishit-singh <rsrishitsingh@gmail.com>
2022-04-01 13:05:26 +0530

    Changed demo plane dimensions.

--- src/scene.ts
+++ src/scene.ts
@@ -128,7 +128,7 @@ export function createScene(renderer: WebGLRenderer) {
   /**
    * Create the plane marker to show on tracked surfaces.
    */
-  const planeMarker: Mesh = new Plane(new Point2D(0.3, 1),"assets/rug.jpg").GeometryMesh;
+  const planeMarker: Mesh = new Plane(new Point2D(1, 1),"assets/rug.jpg").GeometryMesh;
   // planeMarker.rotation.x = Math.PI / 2;
   scene.add(planeMarker);
 
